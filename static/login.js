2df92041157f36f383d0c2068bd9a646f599049f
nevikw39 <nevikw39@icloud.com>
2020-07-18 01:57:19 +0800

    Log-in

--- static/login.js
+++ static/login.js
@@ -1,7 +1,15 @@
-$("#inputPassword").click(() => {
+$("#btnPassword").click(() => {
     let data = new URLSearchParams();
-    data.append("pwd", $("inputPassword").val());
-    fetch("login", { method: "post", body: data }).then(() => window.location.href = "success").catch(e => {
-        console.log(e);
-    })
+    data.append("pwd", $("#inputPassword").val());
+    fetch("login", { method: "post", body: data, credentials: 'include' }).then(r => {
+        switch (r.status) {
+            case 200:
+                window.location.href = "success";
+                break;
+            case 403:
+            case 401:
+                $("#inputPassword").addClass("is-invalid");
+                break;
+        }
+    }).catch(e => console.log(e))
 });
