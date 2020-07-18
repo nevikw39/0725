14c379b6d801e02f6b090a3270531dee7e379bd6
nevikw39 <nevikw39@icloud.com>
2020-07-18 12:11:39 +0800

    login: Show modal of 403

--- static/login.js
+++ static/login.js
@@ -1,15 +1,15 @@
 $("#btnPassword").click(() => {
     let data = new URLSearchParams();
     data.append("pwd", $("#inputPassword").val());
-    fetch("login", { method: "post", body: data, credentials: 'include' }).then(r => {
+    fetch(".", { method: "post", body: data, credentials: 'include' }).then(r => {
         switch (r.status) {
-            case 200:
-                window.location.href = "success";
-                break;
             case 403:
+                $("#modal").modal("show")
             case 401:
                 $("#inputPassword").addClass("is-invalid");
                 break;
+            case 200:
+                window.location.reload();
         }
     }).catch(e => console.log(e))
 });
